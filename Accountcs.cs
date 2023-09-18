f54410294e6ce47da7c12f04193446e3430ec88f
ahmedhh33 <ahmedhh3633@gmail.com>
2023-09-18 09:35:57 +0400

    BS_DB

---
+++ Accountcs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BS_DB
+{
+    public class Account
+    {
+
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public decimal Balance { get; set; }
+        public List<Transaction> TransactionHistory { get; private set; }
+
+
+        public Account()
+        {
+            TransactionHistory = new List<Transaction>();
+        }
+
+        public Account(int accountNumber, string accountHolderName, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            AccountHolderName = accountHolderName;
+            Balance = balance;
+            TransactionHistory = new List<Transaction>();
+        }
+    }
+}
